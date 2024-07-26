9cd263b18e74ae749f069db89923cd44cd760fad
AltLexon <xanonymusx19@gmail.com>
2024-07-26 03:21:10 -0300

    #

--- run.lua
+++ run.lua
@@ -1,3 +1,4 @@
+local rbxmSuite = loadstring(game:HttpGetAsync("https://github.com/richie0866/rbxm-suite/releases/latest/download/rbxm-suite.lua"))()
 local path = rbxmSuite.download("AltLexon/FE2-API@latest", "api.rbxm")
 local model = rbxmSuite.launch(path)
 local API = rbxmSuite.require(model).default;
